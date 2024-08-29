76f92b890cb8d36bb3a735b99658d2abe8f4f339
dieguindeprtg <dieguindeprtg@gmail.com>
2024-08-29 17:54:45 -0400

    Add feature List

---
+++ capitulo_6/resources/views/user/index.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Users</title>
+    </head>
+    <body>
+    
+        <h1>User List</h1>
+    
+        <br>
+        <br>
+    
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Created At</th>
+                    <th>Updated At</th>
+                </tr>
+            </thead>
+    
+            <tbody>
+                @foreach ($users as $user)
+                    <tr>
+                        <td>{{$user->id}}</td>
+                        <td>{{$user->name}}</td>
+                        <td>{{$user->email}}</td>
+                        <td>{{$user->created_at}}</td>
+                        <td>{{$user->updated_at}}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    
+    </body>
+    </html>
